8f656d99474df258810b276fd8eddb870ae54e15
Administrator <admin@example.com>
2020-01-13 21:42:29 +0900

    v9

--- websphere-liberty/app/wasdev/sample/MyServlet.java
+++ websphere-liberty/app/wasdev/sample/MyServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class SimpleServlet
  */
 @WebServlet("/*")
-public class SimpleServlet extends HttpServlet {
+public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
